8f1fa157cddcd65d3fb3b9d8643e96b9324e3117
ZL&EL <lzc123456ab@gmail.com>
2023-07-02 21:42:40 -0400

    2

--- src/main/java/lzc/com/example/modemo/entity/Student.java
+++ src/main/java/lzc/com/example/modemo/entity/Student.java
@@ -3,6 +3,9 @@ package lzc.com.example.modemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "student")
 public class Student {
@@ -18,6 +21,28 @@ public class Student {
     private String  firstName;
     @Column(name = "email")
     private String  email;
+
+    @ManyToMany(fetch = FetchType.LAZY,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinTable(name="course_student",
+            joinColumns = @JoinColumn(name="student_id"),
+            inverseJoinColumns = @JoinColumn(name="course_id"))
+    private List<Course> courses;
+
+    // add convenience method for add
+    public void addCourse(Course theCourse){
+        if(courses==null){
+            courses = new ArrayList<>();
+        }
+        courses.add(theCourse);
+    }
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
     public Student() {
     }
 
