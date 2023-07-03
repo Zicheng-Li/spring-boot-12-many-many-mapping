4a9c429bd6e58958dd36033ee28a0be96442b8fa
ZL&EL <lzc123456ab@gmail.com>
2023-07-02 21:35:50 -0400

    2

--- src/main/java/lzc/com/example/modemo/entity/Course.java
+++ src/main/java/lzc/com/example/modemo/entity/Course.java
@@ -28,6 +28,29 @@ public class Course {
     @JoinColumn(name="course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinTable(name="course_student",
+    joinColumns = @JoinColumn(name="course_id"),
+    inverseJoinColumns = @JoinColumn(name="student_id"))
+    private List<Student> students;
+
+    // add a convenience method
+    public void addStudent(Student theStudent){
+        if(students==null){
+            students=new ArrayList<Student>();
+        }else {
+            students.add(theStudent);
+        }
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     // add a convenience method
     public void addReview(Review theReview){
         if(reviews==null){
